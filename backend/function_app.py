00dd5b2622e66590192d1b507af5ad2b7c6787e0
hitesh68333 <h.manani91@gmail.com>
2026-05-26 16:47:47 +0400

    added visitor counter

--- backend/function_app.py
+++ backend/function_app.py
@@ -1,3 +1,4 @@
+import json
 import azure.functions as func
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
@@ -5,4 +6,11 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="visitor-count", methods=["GET"])
 def visitor_count(req):
-    return func.HttpResponse("Visitor counter API is running")
+    response_body = {
+        "visitor_count": 0
+    }
+
+    return func.HttpResponse(
+        json.dumps(response_body),
+        mimetype="application/json"
+    )
