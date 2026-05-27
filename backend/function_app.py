b8e105321dd2d6ea3ddc3c174f68ac2f928eb052
hitesh68333 <h.manani91@gmail.com>
2026-05-27 20:41:06 +0400

    modified function_app.py, to include edge case for when partition_key and row_key if they dont exisit, it creates them

--- backend/function_app.py
+++ backend/function_app.py
@@ -1,6 +1,7 @@
 import json
 import os
 import azure.functions as func
+from azure.core.exceptions import ResourceNotFoundError
 from azure.data.tables import TableClient, UpdateMode
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
@@ -14,23 +15,33 @@ def visitor_count(req):
         table_name="VisitorCounter"
     )
 
-    counter = table_client.get_entity(
-        partition_key="site",
-        row_key="main"
-    )
+    try:
+        counter = table_client.get_entity(
+            partition_key="site",
+            row_key="main"
+        )
 
-    count_value = counter["Count"]
-    if hasattr(count_value, "value"):
-        count_value = count_value.value
+        count_value = counter["Count"]
+        if hasattr(count_value, "value"):
+            count_value = count_value.value
 
-    current_count = int(count_value)
-    new_count = current_count + 1
-    counter["Count"] = new_count
+        current_count = int(count_value)
+        new_count = current_count + 1
+        counter["Count"] = new_count
 
-    table_client.update_entity(
-        entity=counter,
-        mode=UpdateMode.REPLACE
-    )
+        table_client.update_entity(
+            entity=counter,
+            mode=UpdateMode.REPLACE
+        )
+    except ResourceNotFoundError:
+        new_count = 1
+        table_client.create_entity(
+            entity={
+                "PartitionKey": "site",
+                "RowKey": "main",
+                "Count": new_count
+            }
+        )
 
     response_body = {
         "visitor_count": new_count
