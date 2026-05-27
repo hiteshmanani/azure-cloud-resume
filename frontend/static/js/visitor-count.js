f710d861295b826fbd7779ff53e278ee98d96832
hitesh68333 <h.manani91@gmail.com>
2026-05-27 11:52:17 +0400

    updated visitor-count js code, to  handle local func+ deployed func cases

--- frontend/static/js/visitor-count.js
+++ frontend/static/js/visitor-count.js
@@ -1,8 +1,11 @@
 const visitorCountElement = document.getElementById("visitor-count");
 const isLocalSite = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+const visitorCountApiUrl = isLocalSite
+  ? "http://localhost:7071/api/visitor-count"
+  : "https://func-hm-crc-eaene9aufsf4cmen.uaenorth-01.azurewebsites.net/api/visitor-count";
 
-if (visitorCountElement && isLocalSite) {
-  fetch("http://localhost:7071/api/visitor-count")
+if (visitorCountElement) {
+  fetch(visitorCountApiUrl)
     .then(function (response) {
       return response.json();
     })
